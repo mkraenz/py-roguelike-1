8e509a5eb4158520047523c0f9fa4dd9504ccc20
Mirco Kraenz <software@kraenz.eu>
2025-06-30 09:42:39 +0200

    ci: nuitka windows

--- scripts/windows-install-dependency-walker.py
+++ scripts/windows-install-dependency-walker.py
@@ -2,12 +2,17 @@ import os
 import urllib.request
 
 dir = "C:/Users/RUNNER~1/AppData/Local/Nuitka/Nuitka/Cache/DOWNLO~1/depends/x86_64/"
+# dir = "src/"
+zip_filename = f"{dir}depends22_x64.zip"
 
 urllib.request.urlretrieve(
     "https://www.dependencywalker.com/depends22_x64.zip",
-    f"{dir}depends22_x64.zip",
+    zip_filename,
 )
-os.system(
-    "powershell -command \"Expand-Archive -Force 'C:/Users/RUNNER~1/AppData/Local/Nuitka/Nuitka/Cache/DOWNLO~1/depends/x86_64/depends22_x64.zip' 'C:/Users/RUNNER~1/AppData/Local/Nuitka/Nuitka/Cache/DOWNLO~1/depends/x86_64'\""
+print(f"Downloaded file to {zip_filename}")
+error_code = os.system(
+    f"powershell -command \"Expand-Archive -Force '{zip_filename}' 'C:/Users/RUNNER~1/AppData/Local/Nuitka/Nuitka/Cache/DOWNLO~1/depends/x86_64'\""
 )
+if error_code != 0:
+    raise SystemExit("Installation of Dependency Walker on windows failed.")
 print("Extracted dependency runner")
