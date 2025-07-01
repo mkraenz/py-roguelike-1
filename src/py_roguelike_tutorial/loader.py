fa2c5bcaf88c01352ee34beb6bd1e609240b6218
Mirco Kraenz <software@kraenz.eu>
2025-07-01 09:04:58 +0200

    feat: improved error messaging on malformed item entity defs, refactor DataLoader

--- src/py_roguelike_tutorial/loader.py
+++ src/py_roguelike_tutorial/loader.py
@@ -7,30 +7,41 @@ from py_roguelike_tutorial.components.procgen_config import DungeonTable, Entity
 from py_roguelike_tutorial.entity_deserializers import item_from_dict
 from py_roguelike_tutorial.utils import assets_filepath
 
+
 if TYPE_CHECKING:
     from py_roguelike_tutorial.entity import Item, Actor
 
+type SpawnRateTable = dict[int, list[tuple[str, int]]]
+
 
 class DataLoader:
-    def load_item_entities(self):
+    @staticmethod
+    def load_asset(filename: str):
+        with open(assets_filepath(filename)) as file:
+            return yaml.safe_load(file.read())
+
+    def load_item_entities(self) -> dict[str, Item]:
         items: dict[str, Item] = {}
 
-        # load items
-        with open(assets_filepath("assets/data/entities/items.yml")) as items_file:
-            contents = items_file.read()
-        item_data: dict[str, dict] = yaml.safe_load(contents)
+        filename = "assets/data/entities/items.yml"
+        item_data: dict[str, dict] = self.load_asset(filename)
         # todo consider using pydantic for validation
+        erroneous_keys: list[str] = []
         for key, val in item_data.items():
-            items[key] = item_from_dict(val)
+            try:
+                items[key] = item_from_dict(val)
+            except Exception as e:
+                erroneous_keys.append(key)
+        if erroneous_keys:
+            error_keys = ", ".join(erroneous_keys)
+            raise SystemError(
+                f"Error loading {filename}. Entity definitions malformed: {error_keys}"
+            )
         return items
 
-    def load_item_drops_rates(self, item_prefabs: dict[str, Item]):
-        with open(
-            assets_filepath("assets/data/tables/dungeon_item_drops.yml")
-        ) as dungeon_drops_file:
-            item_drops_data: dict[int, list[list[str | int]]] = yaml.safe_load(
-                dungeon_drops_file.read()
-            )
+    def load_item_drops_rates(self, item_prefabs: dict[str, Item]) -> DungeonTable:
+        filename = "assets/data/tables/dungeon_item_drops.yml"
+        item_drops_data: SpawnRateTable = self.load_asset(filename)
         item_chances: DungeonTable = {}
         for floor, entity_name_table in item_drops_data.items():
             entity_table = map(
@@ -40,14 +51,9 @@ class DataLoader:
             item_chances[floor] = list(entity_table)
         return item_chances
 
-    def load_enemy_spawn_rates(self, npc_prefabs: dict[str, Actor]):
-        # load enemy spawn rates
-        with open(
-            assets_filepath("assets/data/tables/dungeon_enemy_spawns.yml")
-        ) as enemy_spawn_rates_file:
-            raw_spawn_rates_by_floor: dict[int, list[list[str | int]]] = yaml.safe_load(
-                enemy_spawn_rates_file.read()
-            )
+    def load_enemy_spawn_rates(self, npc_prefabs: dict[str, Actor]) -> DungeonTable:
+        filename = "assets/data/tables/dungeon_enemy_spawns.yml"
+        raw_spawn_rates_by_floor: SpawnRateTable = self.load_asset(filename)
         spawn_rates: DungeonTable = {}
         for floor, raw_entity_table in raw_spawn_rates_by_floor.items():
             entity_table = map(
@@ -56,3 +62,8 @@ class DataLoader:
             )
             spawn_rates[floor] = list(entity_table)
         return spawn_rates
+
+    def load_npcs(self) -> dict[str, Actor]:
+        filename = "assets/data/entities/npcs.yml"
+        npcs: dict[str, Actor] = {}
+        return npcs
