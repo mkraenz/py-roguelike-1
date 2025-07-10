93e0fd87696abc80138c1622dab351b7769cc3dd
Mirco Kraenz <software@kraenz.eu>
2025-07-10 16:40:16 +0200

    feat: script to write the yaml behaviortree as a mermaid file (graph visualization)

---
+++ src/py_roguelike_external_tools/behavior-tree-yml-to-mermaid.py
@@ -0,0 +1,38 @@
+import yaml
+
+
+filename = "src/assets/data/experiments/behavior_trees.yml"
+out_filename = "behavior_tree.mermaid"
+
+with open(filename) as file:
+    data = yaml.safe_load(file.read())
+
+bt = data["melee_attacker"]["root"]
+
+
+def draw_tree(tree: dict, start_index: int, subtree_root: str | None) -> list[str]:
+    current_node = f"{start_index}_{tree['type']}"
+    res: list[str] = (
+        [f"{subtree_root} --> {current_node}"]
+        if subtree_root is not None
+        else [current_node]
+    )
+    j = start_index + 1
+    if "children" in tree:
+        for child_tree in tree["children"]:
+            drawn_subtree = draw_tree(child_tree, j, current_node)
+            res += drawn_subtree
+            j += len(drawn_subtree)
+    return res
+
+
+with open(out_filename, "w") as outfile:
+    header = """---
+title: Behavior Tree 
+---
+graph TB"""
+    drawn_tree = draw_tree(bt, 0, None)
+    drawn_tree_indented = [f"    {s}" for s in drawn_tree]
+
+    contents = "\n".join((header, *drawn_tree_indented))
+    outfile.write(contents)
